1311ad1cead50221a187c33ce0f7b19d9bb3ab68
Emad A. Ismael <EmadAIsmael@yahoo.com>
2021-02-22 00:02:40 +0200

    Mon, Feb. 22nd 2021 00:02

--- Cinema Room Manager/task/src/cinema/Cinema.kt
+++ Cinema Room Manager/task/src/cinema/Cinema.kt
@@ -1,34 +1,78 @@
 package cinema
 
+
+class Cinema(private val rows: Int, private val seatsPerRow: Int) {
+
+    data class TicketPrice(val front: Int, val back: Int)
+
+    private var seating = Array(rows) { Array(seatsPerRow) { "S" } }
+    private val frontTicketPrice = 10
+    private val backTicketPrice = 8
+    private val totalSeats = rows * seatsPerRow
+    private var ticketPrice: TicketPrice = TicketPrice(10, 10)
+    private var profit = 0
+
+    init {
+        ticketPrice = if (totalSeats <= 60) {
+            TicketPrice(frontTicketPrice, frontTicketPrice)
+        } else {
+            TicketPrice(frontTicketPrice, backTicketPrice)
+        }
+    }
+
+    fun book(row: Int, seat: Int) {
+        val price = getTicketPrice(row)
+        println("\nTicket price: $$price\n")
+        seating[row - 1][seat - 1] = "B"
+    }
+
+    private fun isFrontRow(row: Int): Boolean = row in 1..(rows / 2)
+
+    private fun getTicketPrice(row: Int) =
+        if (isFrontRow(row)) ticketPrice.front
+        else ticketPrice.back
+
+    fun calculateProfit(): Int {
+        val frontRows = rows / 2
+        val backRows = rows - frontRows
+
+        profit = (frontRows * seatsPerRow) * ticketPrice.front +
+                (backRows * seatsPerRow) * ticketPrice.back
+        return profit
+    }
+
+    override fun toString(): String {
+        var layout = ""
+        layout += "Cinema:\n  "
+        layout += (1..seatsPerRow).joinToString(" ") + "\n"
+        for (row in 1..rows) {
+            layout += "$row "
+            for (seat in 1..seatsPerRow)
+                layout += "${seating[row - 1][seat - 1]} "
+            layout += "\n"
+        }
+        return layout
+    }
+}
+
+
 fun main() {
     println("Enter the number of rows:")
     val rows = readLine()!!.toInt()
     println("Enter the number of seats in each row:")
     val seatsPerRow = readLine()!!.toInt()
-    var profit = 0
 
-    val totalSeats = rows * seatsPerRow
-    if (totalSeats <= 60) {
-        val ticket = 10
-        profit = totalSeats * ticket
-    }
-    else {
-        val frontRows = rows / 2
-        val backRows = rows - frontRows
-        val frontTickets = 10
-        val backTickets = 8
-        profit = (frontRows * seatsPerRow) * frontTickets + (backRows * seatsPerRow) * backTickets
-    }
+    val cinema = Cinema(rows, seatsPerRow)
+    println(cinema)
 
-    println("Total income:\n$$profit")
+    println("Enter a row number:")
+    val row = readLine()!!.toInt()
+    println("Enter a seat number in that row:")
+    val seat = readLine()!!.toInt()
 
-//    println("Cinema:")
-//    print("  ")
-//    println((1..seatsPerRow).joinToString(" "))
-//    for (row in 1..rows) {
-//        print("$row ")
-//        for (seat in 1..seatsPerRow)
-//            print("S ")
-//        println()
-//    }
+    cinema.book(row, seat)
+    println(cinema)
+
+//    val profit: Int = cinema.calculateProfit()
+//    println("Total income:\n$$profit")
 }
