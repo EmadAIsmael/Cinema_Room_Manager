77bc91b960894e6e3188bd36006033a6d2fedb83
Emad A. Ismael <EmadAIsmael@yahoo.com>
2021-02-22 00:51:03 +0200

    Mon, Feb. 22nd 2021 00:50

--- Cinema Room Manager/task/src/cinema/Cinema.kt
+++ Cinema Room Manager/task/src/cinema/Cinema.kt
@@ -1,6 +1,5 @@
 package cinema
 
-
 class Cinema(private val rows: Int, private val seatsPerRow: Int) {
 
     data class TicketPrice(val front: Int, val back: Int)
@@ -20,9 +19,9 @@ class Cinema(private val rows: Int, private val seatsPerRow: Int) {
         }
     }
 
-    fun book(row: Int, seat: Int) {
+    private fun book(row: Int, seat: Int) {
         val price = getTicketPrice(row)
-        println("\nTicket price: $$price\n")
+        println("Ticket price: $$price")
         seating[row - 1][seat - 1] = "B"
     }
 
@@ -41,15 +40,44 @@ class Cinema(private val rows: Int, private val seatsPerRow: Int) {
         return profit
     }
 
+    fun menu() {
+        while (true) {
+            var selection = 10
+            while (selection !in arrayOf(0, 1, 2)) {
+                println(
+                    """
+                1. Show the seats
+                2. Buy a ticket
+                0. Exit
+            """.trimIndent()
+                )
+
+                selection = readLine()!!.toInt()
+            }
+            when (selection) {
+                1 -> println(this)
+                2 -> {
+                    println("Enter a row number:")
+                    val row = readLine()!!.toInt()
+                    println("Enter a seat number in that row:")
+                    val seat = readLine()!!.toInt()
+
+                    book(row, seat)
+                }
+                0 -> return             // exitProcess(0)
+            }
+        }
+    }
+
     override fun toString(): String {
-        var layout = ""
+        var layout = "\n"
         layout += "Cinema:\n  "
         layout += (1..seatsPerRow).joinToString(" ") + "\n"
         for (row in 1..rows) {
             layout += "$row "
             for (seat in 1..seatsPerRow)
                 layout += "${seating[row - 1][seat - 1]} "
-            layout += "\n"
+             layout += "\n"
         }
         return layout
     }
@@ -63,15 +91,7 @@ fun main() {
     val seatsPerRow = readLine()!!.toInt()
 
     val cinema = Cinema(rows, seatsPerRow)
-    println(cinema)
-
-    println("Enter a row number:")
-    val row = readLine()!!.toInt()
-    println("Enter a seat number in that row:")
-    val seat = readLine()!!.toInt()
-
-    cinema.book(row, seat)
-    println(cinema)
+    cinema.menu()
 
 //    val profit: Int = cinema.calculateProfit()
 //    println("Total income:\n$$profit")
