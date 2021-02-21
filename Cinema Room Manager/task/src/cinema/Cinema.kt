5ed5af9b997af726ee5e8fd149a4e03fa339eb08
Emad A. Ismael <EmadAIsmael@yahoo.com>
2021-02-21 22:54:28 +0200

    Sun, Feb. 21st 2021 22:54

--- Cinema Room Manager/task/src/cinema/Cinema.kt
+++ Cinema Room Manager/task/src/cinema/Cinema.kt
@@ -1,16 +1,34 @@
 package cinema
 
 fun main() {
-    val rows = 7
-    val seatsPerRow = 8
+    println("Enter the number of rows:")
+    val rows = readLine()!!.toInt()
+    println("Enter the number of seats in each row:")
+    val seatsPerRow = readLine()!!.toInt()
+    var profit = 0
 
-    println("Cinema:")
-    print("  ")
-    println((1..seatsPerRow).joinToString(" "))
-    for (row in 1..rows) {
-        print("$row ")
-        for (seat in 1..seatsPerRow)
-            print("S ")
-        println()
+    val totalSeats = rows * seatsPerRow
+    if (totalSeats <= 60) {
+        val ticket = 10
+        profit = totalSeats * ticket
     }
+    else {
+        val frontRows = rows / 2
+        val backRows = rows - frontRows
+        val frontTickets = 10
+        val backTickets = 8
+        profit = (frontRows * seatsPerRow) * frontTickets + (backRows * seatsPerRow) * backTickets
+    }
+
+    println("Total income:\n$$profit")
+
+//    println("Cinema:")
+//    print("  ")
+//    println((1..seatsPerRow).joinToString(" "))
+//    for (row in 1..rows) {
+//        print("$row ")
+//        for (seat in 1..seatsPerRow)
+//            print("S ")
+//        println()
+//    }
 }
